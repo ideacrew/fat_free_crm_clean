8606ac75f8a9694b3e4ad8d76351bd0446bef287
Bill Transue <transue@gmail.com>
2020-04-28 14:05:59 -0400

    Move accessors to module for Top Level Entities

--- app/decorators/encrypted_field_decorator.rb
+++ app/decorators/encrypted_field_decorator.rb
@@ -5,14 +5,38 @@ module EncryptedFieldDecorator
   end
 end
 
-class Contact
-  Field.joins(:field_group).where(as: 'encrypted_field' , field_groups: { klass_name: 'Contact' }).each do |field|
-    define_method "#{field.name}=" do |value|
-      write_attribute field.name, SymmetricEncryption.encrypt(value)
-    end
+module EncryptedFieldAccessors
+  extend ActiveSupport::Concern
 
-    define_method field.name do
-      SymmetricEncryption.decrypt(read_attribute(field.name))&.gsub(/.(?=.{4})/,'*')
+  included do
+    Field.joins(:field_group).where(as: 'encrypted_field' , field_groups: { klass_name: 'Contact' }).each do |field|
+      define_method "#{field.name}=" do |value|
+        write_attribute field.name, SymmetricEncryption.encrypt(value)
+      end
+
+      define_method field.name do
+        SymmetricEncryption.decrypt(read_attribute(field.name))&.gsub(/.(?=.{4})/,'*')
+      end
     end
   end
+end
+
+class Account
+  include EncryptedFieldAccessors
+end
+
+class Campagin
+  include EncryptedFieldAccessors
+end
+
+class Contact
+  include EncryptedFieldAccessors
+end
+
+class Lead
+  include EncryptedFieldAccessors
+end
+
+class Opportunity
+  include EncryptedFieldAccessors
 end
